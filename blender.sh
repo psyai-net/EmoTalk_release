8c9c4c75631d5dda5e8c391b20890d032ef8041e
Jungwoo Choi <dnp4vr@gmail.com>
2023-09-21 19:08:03 +0900

    Install command for blender

---
+++ blender.sh
@@ -0,0 +1,4 @@
+wget https://mirror.freedif.org/blender/release/Blender3.4/blender-3.4.1-linux-x64.tar.xz
+tar -xf blender-3.4.1-linux-x64.tar.xz
+mv blender-3.4.1-linux-x64 blender && rm blender-3.4.1-linux-x64.tar.xz
+
