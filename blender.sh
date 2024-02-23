6ad7913f596485621c4d4f20c70e8efa316e7a1b
ZiqiaoPeng <93774583+ZiqiaoPeng@users.noreply.github.com>
2024-02-23 11:08:33 +0800

    Update blender.sh
    Update blender download link.

--- blender.sh
+++ blender.sh
@@ -1,4 +1,4 @@
-wget https://mirror.freedif.org/blender/release/Blender3.4/blender-3.4.1-linux-x64.tar.xz
+wget https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.4/blender-3.4.1-linux-x64.tar.xz
 tar -xf blender-3.4.1-linux-x64.tar.xz
 mv blender-3.4.1-linux-x64 blender && rm blender-3.4.1-linux-x64.tar.xz
 
